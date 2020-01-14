309351a91430ce388555f53fbe35471e6fb30fe0
Ali Nur <alinurcowal@gmail.com>
2020-01-14 16:02:13 +0700

    first-commit

---
+++ CobaStruk.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void toko() 
+{
+    cout << "Domino's Pizza Galuh Mas 91954" << endl;
+}
+
+void alamat_pusat()
+{
+    cout << "Sahid Sudirman Center Lt.27" << endl;
+    cout << "Jl. Jend Sudirman Kav.86 Jakarta" << endl;
+    cout << "ABN: NPWP: 02.505.566.6-022.00" << endl;
+}
+
+void PT()
+{
+    cout << "PT. Dom Pizza Indonesia" << endl;
+    cout << "www.dominos.co.id" << endl;
+}
+
+int main()
+{
+    //cout << "Program Struk Belanja!" << endl;
+    return 0;
+}
